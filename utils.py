6bbff82baab843661127d2a90039a1347febe64a
Zhao-Qihan <qihanzhao21@gmail.com>
2025-07-11 11:13:04 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ def qa_agent(question, memory, api_key):
 
     #嵌入
     embeddings_model = OpenAIEmbeddings(model = "text-embedding-3-large",
-                                        openai_api_key = "sk-CjAByE6LjIdzvREetoa9sGPkpqXUtdLqAnLGHS5Qma0EbmCV",
+                                        openai_api_key = "sk-k3M6X4FHF5GCyNnt6c2aD88122Ab40EaA91bB7Ce50B7EeA6",
                                         openai_api_base = "https://api.aigc369.com/v1")
 
     #储存
